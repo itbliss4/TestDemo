f783c97fbb4f52fefd9baf998f2959f4eab8153d
macserver <macserver@macservers-mac-pro.local>
2017-05-10 11:11:30 +0530

    Initial Commit

---
+++ TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by macserver on 5/10/17.
+//  Copyright © 2017 macserver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by macserver on 5/10/17.
+//  Copyright © 2017 macserver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
